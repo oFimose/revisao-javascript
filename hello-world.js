371161fea9798a5bff7a4457c37ea6dec341b27b
Fimose devups <fimosedaprog@gmail.com>
2025-09-24 21:43:58 -0300

    SQL TESTES
    codigos banco dados

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,36 @@
 console.log("hello world");
+
+-- selecionar todas as colunas dos clientes
+select *
+	from Cliente;
+
+-- selecionar apenas nomes e emails dos clientes
+select nome, email
+	from Cliente;
+
+-- selecionar todas as colunas onde o nome do cliente é Maria Souza
+select * 
+from Cliente
+where nome = 'Maria Souza'
+
+-- selecionar todas as colunas onde o nome é 3
+select *
+from cliente
+where id = 3;
+
+-- selecionar todas as colunas ordenando de forma ascendente pela DataNascimento
+select *
+from cliente
+order by DataNascimento asc;
+
+-- selecionar todas as colunas ordenando de forma descente pelo id
+select * 
+from Cliente
+order by id desc;
+
+update cliente
+set Email = 'maria.souza@gmail.com'
+where Nome = 'Maria Souza'
+
+select *
+from cliente
