33e49deacf46494180cb5b43964b37c2fc1357ad
Fimose devups <fimosedaprog@gmail.com>
2025-09-26 21:37:41 -0300

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -70,3 +70,43 @@ where Id = 5
 
 delete from funcionarios
 where id = 2;
+
+-------
+
+
+create table pedido (
+Id int primary key identity(1,1),
+NomeCliente nvarchar(100) not null,
+DataPedido datetime not null default getdate(),
+ValorTotal decimal(10,2) not null,
+Status varchar(20) not null default 'Pendente',
+FormaPagamento varchar(30) null, 
+Observacao nvarchar(200) null
+);
+
+insert into pedido (NomeCliente, DataPedido, ValorTotal, Status, FormaPagamento, Observacao)
+values 
+('Pedrinho', '2025-05-21', 10.00, 'Aguardando', 'dinhero vivo', 'sem maionese'),
+('Teleco', '2025-05-13', 150.00, 'Aprovado', 'pix', 'bem passado'),
+('Borges', '2025-05-14', 100.00, 'Aguardando', 'pix', 'sou doente, mas n importa'),
+('Bilau', '2025-05-30', 0, 'Cancelado', 'debito', '...');
+
+select * from pedido
+order by ValorTotal desc;
+
+update pedido
+set ValorTotal = '299.99'
+where id = 2;
+
+delete from pedido where id = 5
+
+select * from pedido
+where FormaPagamento = 'pix'
+order by DataPedido desc;
+
+select * from pedido
+where LOWER(Observacao) like 'sou doente, mas n importa'
+
+select top 2 *
+from pedido 
+order by ValorTotal desc;
